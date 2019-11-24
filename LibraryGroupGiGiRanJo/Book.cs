8adc8906ab12c3f9380e0789418a048cc9b34a73
Joseph Harburg <josephharburg@gmail.com>
2019-11-23 20:11:39 -0500

    Changed styling and add text to file

--- LibraryGroupGiGiRanJo/Book.cs
+++ LibraryGroupGiGiRanJo/Book.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace LibraryGroupGiGiRanJo
 {
     class Book
     {
+        private string title;
+        private string author;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
         public Book() { }
+
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+        }
+
+        public static void BookToTxtFile(List<Book> books)
+        {
+            StreamWriter bks = new StreamWriter(@"C:\Users\josep\source\repos\GroupLab\LibraryMidGroup\LibraryGroupGiGiRanJo\Book.txt"); 
+
+            foreach(Book book in books)
+            {
+                string csv = $"{book.Title},{book.Author}";
+                bks.WriteLine(csv);
+            }
+
+            bks.Close();
+        }
             
     }
 }
